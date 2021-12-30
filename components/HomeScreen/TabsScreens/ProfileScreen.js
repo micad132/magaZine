e341ff94f16df35daf06fd166b5dd561d7e61926
LeeTomahawk <bartek20122013@gmail.com>
2021-12-30 16:44:45 +0100

    Add sign out button to profile screen

--- components/HomeScreen/TabsScreens/ProfileScreen.js
+++ components/HomeScreen/TabsScreens/ProfileScreen.js
@@ -1,18 +1,27 @@
-import { Text,Box } from "native-base";
+import { Text, Box } from "native-base";
 import React from "react";
+import { auth } from "../../../firebase";
+import { TouchableOpacity } from "react-native";
 
-
-const ProfileScreen = () =>{
-
-    
-    return(
-    
-    
-    
-    <Box w="100%" h="100%" bgColor="rgb(41,54,63)">
-        <Text color="#fff" mx="5" my="10">ProfileScreen</Text>
-    </Box>
+const ProfileScreen = ({navigation}) => {
+    const handleSignOut = () => {
+        auth
+          .signOut()
+          .then(() => {
+            navigation.replace("Login");
+          })
+          .catch((error) => alert(error.message));
+    }
+    return (
+        <Box w="100%" h="100%" bgColor="rgb(41,54,63)">
+        <Text color="#fff" mx="5" my="10">
+            ProfileScreen
+        </Text>
+        <TouchableOpacity onPress={handleSignOut}>
+            <Text color="#fff">Sign Out</Text>
+        </TouchableOpacity>
+        </Box>
     );
-}
+};
 
 export default ProfileScreen;
