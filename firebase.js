804c4d23241094b92a32292d5015b0b090655fbd
LeeTomahawk <bartek20122013@gmail.com>
2021-12-30 15:41:02 +0100

    Remove globalcontext, add firebase config

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore, collection, getDocs} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLSWsDLJqDyIxVMZE2zAMginRFBm_zskE",
+    authDomain: "magazine-14972.firebaseapp.com",
+    projectId: "magazine-14972",
+    storageBucket: "magazine-14972.appspot.com",
+    messagingSenderId: "867751473428",
+    appId: "1:867751473428:web:85331874748e6886d61201"
+  };
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+const db = getFirestore();
+
+export { auth,colref};
